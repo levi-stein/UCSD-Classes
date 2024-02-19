b1459d387d5450e001246707a5cc826134df14b8
levi-stein <levihstein@gmail.com>
2024-02-18 21:33:19 -0800

    finishing hw4

--- WI 24/MAE 143A/HW4/hw4.m
+++ WI 24/MAE 143A/HW4/hw4.m
@@ -1,11 +1,11 @@
 clear all; close all; clc;
 %% Problem 1
-% F = RR_tf([1 -1],[1 1]); figure(1); RR_bode(F);
+F = RR_tf([1 -1],[1 1]); figure(1); RR_bode(F); figure(2); RR_step(F);
 %% Problem 2
 w = 1;
-F2a = RR_tf([w^2],[1 .77*w w^2]);
-F2b = RR_tf([w^2],[1 1.85*w w^2]);
+F2a = RR_tf([w^2],[1 .77*w w^2]); figure(3); RR_bode(F2a);
+F2b = RR_tf([w^2],[1 1.85*w w^2]); figure(4); RR_bode(F2b);
 F4 = F2a*F2b;
-RR_step(F4)
-
-%% Problem 3
+figure(5);
+RR_step(F4);
+figure(6); RR_bode(F4);
